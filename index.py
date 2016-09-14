61b9b67abf74f5bbed7eb1dc41c9472ef4d5dfa4
Shreyansh Khajanchi <shreyansh_k@live.com>
2016-09-14 13:58:57 +0530

    fix type, implement barebone sql logic

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask("CourierManagement")
 
 @app.route("/")
-def hello_world():
+def welcome_page():
   return "Welcome to VIT's courier Management System"
 
 @app.route('/track')
@@ -10,11 +10,19 @@ def track_home():
     return "this is where we ask user for courier id"
 
 @app.route('/track/<courierid>')
-def tacking_page(courierid):
-    return "this is where we track courier with id " + courierid
+def tracking_page(courierid):
+    def find_courier(courierid):
+        #sql logic here, put result in result variable
+        return "this is where we track courier with id " + courierid
+    result = find_courier(courierid)
+    return result
 
 @app.route('/devs')
 def developers():
     return "Made with <3 by Shreyansh & Vijay for VIT University"
+
+@app.route("/search")
+def search_home():
+    return "Here we implement search logic"
 
 app.run()
